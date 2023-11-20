85ab2c50a248b9a075ec3604fea52ba5075bf90d
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-11-20 11:02:59 +0700

    .

--- components/products/CategoryModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-
-const schema = new Schema({
-    name: { type: String}, // Tiêu đề (bắt buộc phải có)
-});
-// Đảm bảo rằng bảng Products chưa được định nghĩa trước đó
-module.exports = mongoose.model('category', schema, 'category');
--- config/Config.js
+++
@@ -1,4 +0,0 @@
-const CONSTANTS ={
-    IP:'192.168.1.73',
-}
-module.exports={CONSTANTS};
